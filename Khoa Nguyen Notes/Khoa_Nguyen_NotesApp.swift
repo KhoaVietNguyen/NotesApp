70f674ed3d6725f6997afdcafa9e6f4fec9e35db
altek.global <altek.globals@gmail.com>
2023-11-16 10:18:00 +0700

    Initial Commit

---
+++ Khoa Nguyen Notes/Khoa_Nguyen_NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Khoa_Nguyen_NotesApp.swift
+//  Khoa Nguyen Notes
+//
+//  Created by ALTEK on 16/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct Khoa_Nguyen_NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
